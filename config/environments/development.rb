189e26ab5129fce6d0942782d7c7aad42e413d2f
Daniel Gutierrez <danielgudu22@gmail.com>
2021-10-23 14:55:40 -0500

    Edit of register and log in views, add created_at and mail sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "danielgudu22@gmail.com",
+    password: "Daniel020900",
+    domain: "localhost:3000",
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+  # enable_starttls_auto: true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
